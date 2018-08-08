6c6a8e20d199a850baa85e3d01a8cb6829bb4eea
Aravinder Reddy K <aravinder.k@gmail.com>
2018-08-08 13:23:38 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { DetailsComponent } from './details/details.component';
+import { BrandDetailsComponent } from './brand/brandDetails.component';
+import { AddBrandComponent } from './brand/addBrand.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: HomeComponent
-  },
-  {
-    path: 'details/:id',
-    component: DetailsComponent
-  }
+  {path: 'home', component: HomeComponent},
+  {path: 'brand', component: BrandDetailsComponent},
+  {path: 'add-brand', component: AddBrandComponent},
+  {path: 'edit-brand', component: AddBrandComponent},
+  {path: '', redirectTo: '/home', pathMatch: 'full'}
 ];
 
 @NgModule({
