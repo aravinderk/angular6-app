f30432b2710f8cd569ebbd4e1c50e371f11bae90
Aravinder Reddy K <aravinder.k@gmail.com>
2018-08-08 13:31:41 +0530

    Create addBrand.component.ts

---
+++ src/app/brand/addBrand.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-add-brand',
+  templateUrl: './addBrand.component.html',
+  styleUrls: ['./brand.component.scss']
+})
+export class AddBrandComponent implements OnInit {
+  ngOnInit() {
+
+  }
+
+}
