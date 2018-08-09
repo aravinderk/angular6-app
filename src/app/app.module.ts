786686ac139b2cc6c11192c2d47b0a69f34b3433
Aravinder Reddy K <aravinder.k@gmail.com>
2018-08-09 17:45:53 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,13 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MenuComponent } from './menu/menu.component';
 import { BrandDetailsComponent } from './brand/brandDetails.component';
 import { AddBrandComponent } from './brand/addBrand.component';
+import { DeleteBrandComponent } from './brand/deleteBrand.component';
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
@@ -18,13 +19,15 @@ import { HomeComponent } from './home/home.component';
     MenuComponent,
     HomeComponent,
     AddBrandComponent,
-    BrandDetailsComponent
+    BrandDetailsComponent,
+    DeleteBrandComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
