8454da4da457481fa270035d2a3119fbc79c4829
Aravinder Reddy K <aravinder.k@gmail.com>
2018-08-09 17:45:27 +0530

    Update data.service.ts

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,14 +1,47 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class DataService {
 
-  constructor(private http: HttpClient) { }
+  BRAND_URL = '../assets/data/getBrand.json';
 
-  getBrandDetails() {
-    return this.http.get('../assets/data/getBrand.json');
+  constructor(private http: HttpClient) { }
+  private brandObj = new BehaviorSubject(this.getInitialBrandObj('NEW'));
+  brandDetailsObj = this.brandObj.asObservable();
+
+  getInitialBrandObj(type) {
+    return {
+      brandAttributes: {
+        contentType: [{emailInfo: {from: {}, options: {}, replyTo: {}}}]
+      },
+      brandMatchingAttributes: {address: {}},
+      additionalAttributes: {},
+      brandType: type
+    };
+  }
+
+  setBrandDetails(brandObj) {
+    this.brandObj.next(brandObj);
+  }
+
+  getBrandDetails(brandId) {
+    return this.http.get(`${this.BRAND_URL}?${brandId}`);
+  }
+
+  deleteBrand(brandId) {
+    return this.http.delete(`${this.BRAND_URL}/${brandId}`);
+  }
+
+  addBrand(brandObj) {
+    return this.http.post(this.BRAND_URL, brandObj);
+  }
+
+  updateBrand(brandObj) {
+    return this.http.put(`${this.BRAND_URL}/${brandObj.brandId}`, brandObj);
   }
 }
