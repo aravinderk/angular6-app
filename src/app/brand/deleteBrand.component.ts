968f49617b901520199c988051a67c7bb1ae220c
Aravinder Reddy K <aravinder.k@gmail.com>
2018-08-09 17:49:59 +0530

    Create deleteBrand.component.ts

---
+++ src/app/brand/deleteBrand.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { DataService } from '../data.service';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-delete-brand',
+  templateUrl: './deleteBrand.component.html',
+  styleUrls: ['./brand.component.scss']
+})
+export class DeleteBrandComponent implements OnInit {
+    @Input() show: boolean;
+    @Input() deleteBrandCallback: Function;
+    constructor(private data: DataService, private router: Router) { }
+    ngOnInit() { }
+
+    deleteBrand(brandId) {
+        this.data.deleteBrand(brandId).subscribe(
+            data => {
+                this.deleteBrandCallback();
+            }
+        );
+    }
+
+}
