8b9771bfa26e7bee8bbb6740996b59163c819c78
Aravinder Reddy K <aravinder.k@gmail.com>
2018-08-09 17:48:06 +0530

    Update addBrand.component.ts

--- src/app/brand/addBrand.component.ts
+++ src/app/brand/addBrand.component.ts
@@ -8,8 +8,38 @@ import { Observable } from 'rxjs';
   styleUrls: ['./brand.component.scss']
 })
 export class AddBrandComponent implements OnInit {
+  brandDetailsObj: object;
+  constructor(private data: DataService) { }
   ngOnInit() {
-
+    this.data.brandDetailsObj.subscribe(obj => {
+      this.brandDetailsObj = obj;
+    });
   }
 
+  addBrandAttribute() {
+    this.brandDetailsObj.brandAttributes.contentType.push({emailInfo: {from: {}, options: {}, replyTo: {}}});
+  }
+
+  deleteBrandAttribute(index) {
+    this.brandDetailsObj.brandAttributes.contentType.splice(index, 1);
+  }
+
+  resetBrandForm(type) {
+    const obj = this.data.getInitialBrandObj(type);
+    this.data.setBrandDetails(obj);
+  }
+
+  addOrUpdateBrand() {
+    if (this.brandDetailsObj.brandType === 'EXISTING') {
+      this.data.updateBrand(this.brandDetailsObj).subscribe(
+        data => {}
+      );
+    } else {
+      this.data.addBrand(this.brandDetailsObj).subscribe(
+        data => {
+          this.resetBrandForm('NEW');
+        }
+      );
+    }
+  }
 }
