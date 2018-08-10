057b9daf3d770aa92055a8bcacd2c7c2e0ae2160
Aravinder Reddy K <aravinder.k@gmail.com>
2018-08-10 04:10:27 -0400

    Update data.service.ts

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -2,29 +2,45 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject } from 'rxjs';
 
+export const defaultBrandObj: Brand = {
+  brandId: '',
+  brandName: '',
+  status: '',
+  brandType: '',
+  brandAttributes: {
+    contentType: [{emailInfo: {from: {}, options: {}, replyTo: {}}}]
+  },
+  brandMatchingAttributes: {address: {}},
+  additionalAttributes: {}
+};
+
+interface BrandAttributes {
+  contentType: Array<object>;
+}
+
+export interface Brand {
+  brandAttributes: BrandAttributes;
+  brandMatchingAttributes: object;
+  additionalAttributes: object;
+  brandId: string;
+  brandName: string;
+  status: string;
+  brandType: string;
+}
+
 @Injectable({
   providedIn: 'root'
 })
 
 export class DataService {
 
-  BRAND_URL = '../assets/data/getBrand.json';
-  // BRAND_URL = 'https://u9tticv9ta.execute-api.us-east-1.amazonaws.com/DEV/v1/ipmapi/brand'
+  // BRAND_URL = '../assets/data/getBrand.json';
+  BRAND_URL = 'https://u9tticv9ta.execute-api.us-east-1.amazonaws.com/DEV/v1/ipmapi/brand';
 
   constructor(private http: HttpClient) { }
-  private brandObj = new BehaviorSubject(this.getInitialBrandObj('NEW'));
+  private obj: Brand;
+  private brandObj = new BehaviorSubject(this.obj);
   brandDetailsObj = this.brandObj.asObservable();
-
-  getInitialBrandObj(type) {
-    return {
-      brandAttributes: {
-        contentType: [{emailInfo: {from: {}, options: {}, replyTo: {}}}]
-      },
-      brandMatchingAttributes: {address: {}},
-      additionalAttributes: {},
-      brandType: type
-    };
-  }
 
   setBrandDetails(brandObj) {
     this.brandObj.next(brandObj);
